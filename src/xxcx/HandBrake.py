2431ae1808eaa3f13bc54df88be443557b6ffd5c
yucef sourani <yucefsourani@users.noreply.github.com>
2023-03-20 10:57:08 +0200

    Update HandBrake.py

--- src/xxcx/HandBrake.py
+++ src/xxcx/HandBrake.py
@@ -21,42 +21,36 @@
 #  MA 02110-1301, USA.
 #  
 #  
-from universalplugin.uplugin import BasePlugin
-from utils import get_uniq_name,write_to_tmp
+from arfedora_welcome.classesplugin import BasePlugin
+from arfedora_welcome.utils import get_uniq_name,write_to_tmp
 import subprocess
-import time
-import os
-from urllib import request
-from gi.repository import GLib
-import subprocess
-import tempfile
-import queue
+
 
 if_true_skip         = False
-if_false_skip        = True
-if_one_true_skip     = [False,False]
-if_all_true_skip     = [True,False]
+type_                = "installer"
+arch                 = ("all",)
+distro_name          = ("all",)
+distro_version       = ("all",)
+category             = "Multimedia"
+category_icon_theme  = "applications-multimedia-symbolic"
+desktop_env          = ("all",)
+display_type         = ("all",)
+title                = "HandBrake"
+subtitle             = "Video Transcoder\n(Flatpak User Wide)"
+keywords             = "handbrake converter video"
+licenses             = (("License\nGPL V2.0+","https://www.gnu.org/licenses/old-licenses/gpl-2.0.html"),)
+website              = ("WebSite","https://handbrake.fr/")
                 
-arch                 = ["all"]
-distro_name          = ["all"]
-distro_version       = ["all"]
-category             = "<b>Multimedia</b>"
-category_icon_theme  = "applications-multimedia"
-
 
 
 class Plugin(BasePlugin):
     __gtype_name__ = get_uniq_name(__file__) #uniq name and no space
-    def __init__(self,parent):
+    def __init__(self,parent,threads):
         BasePlugin.__init__(self,parent=parent,
-                            spacing=2,
-                            margin=10,
+                            threads=threads,
                             button_image="HandBrake_Icon.png",
                             button_install_label="Install HandBrake (Flatpak User wide)",
                             button_remove_label="Remove HandBrake (Flatpak User wide)",
-                            buttontooltip="Video Transcoder",
-                            buttonsizewidth=100,
-                            buttonsizeheight=100,
                             button_frame=False,
                             blockparent=False,
                             waitmsg="Wait...",
@@ -68,7 +62,7 @@ class Plugin(BasePlugin):
                             ifremovesucessmsg="Remove HandBrake Done",
                             beforeinstallyesorno="Start Install HandBrake ?",
                             beforeremoveyesorno="Start Remove HandBrake ?",
-                            expand=False,
+                            parallel_install = False,
                             daemon=True)
 
         self.parent = parent
