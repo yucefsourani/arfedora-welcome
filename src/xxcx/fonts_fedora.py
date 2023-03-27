806769fc32ae7735c917dc0b27c9d96636d0bfee
yucef sourani <yucefsourani@users.noreply.github.com>
2023-03-27 11:31:13 +0300

    Update fonts_fedora.py

--- src/xxcx/fonts_fedora.py
+++ src/xxcx/fonts_fedora.py
@@ -21,11 +21,25 @@
 #  MA 02110-1301, USA.
 #  
 #  
-from universalplugin.uplugin import BasePlugin
-from utils import get_uniq_name,write_to_tmp
+from arfedora_welcome import utils
+from arfedora_welcome.classesplugin import BasePlugin
+from arfedora_welcome.utils import get_uniq_name,write_to_tmp
 import subprocess
-import time
-import os
+
+if_true_skip         = False
+type_                = "installer"
+arch                 = ("all",)
+distro_name          = ("fedora",)
+distro_version       = ("all",)
+category             = "System"
+category_icon_theme  = "applications-system-symbolic"
+desktop_env          = ("all",)
+display_type         = ("all",)
+title                = "Fonts"
+subtitle             = "Fonts For Arabic Language"
+keywords             = "font"
+licenses             = (("License\nUNKNOWN",""),)
+website              = ()
 
 all_package_to_remove = [ 'kacst-art-fonts', 'kacst-book-fonts',
                           'kacst-decorative-fonts',
@@ -61,30 +75,16 @@ all_package_to_install = ['dejavu-sans-fonts', 'dejavu-sans-mono-fonts',
                           'google-noto-naskh-arabic-ui-fonts', 
                           'google-noto-sans-old-south-arabian-fonts']
                
-if_true_skip         = False
-if_false_skip        = True
-if_one_true_skip     = [False,False]
-if_all_true_skip     = [True,False]
-                
-arch                 = ["all"]
-distro_name          = ["fedora"]
-distro_version       = ["all"]
-category             = "<b>System</b>"
-category_icon_theme  = "applications-system"
 
 
 class Plugin(BasePlugin):
     __gtype_name__ = get_uniq_name(__file__) #uniq name and no space
-    def __init__(self,parent):
+    def __init__(self,parent,threads):
         BasePlugin.__init__(self,parent=parent,
-                            spacing=2,
-                            margin=10,
+                            threads=threads,
                             button_image="kacsttitle.gif",
-                            button_install_label="Install Arabic Fonts",
-                            button_remove_label="Remove Arabic Fonts",
-                            buttontooltip="Install Remove Arabic Fonts",
-                            buttonsizewidth=100,
-                            buttonsizeheight=100,
+                            button_install_label="Install",
+                            button_remove_label="Remove",
                             button_frame=False,
                             blockparent=False,
                             daemon=True,
@@ -93,30 +93,26 @@ class Plugin(BasePlugin):
                             loadingmsg="Loading...",
                             ifinstallfailmsg="Install Arabic Fonts Failed",
                             ifremovefailmsg="Remove Arabic Fonts Failed",
-                            expand=False)
+                            parallel_install=False)
 
 
         
         
     def check(self):
-        check_package = all([self.check_package(pack) for pack in all_package_to_install])
+        check_package = all([utils.check_rpm_package_exists(pack) for pack in all_package_to_install])
         return not check_package
         
     def install(self):
-        to_install = [pack for pack in all_package_to_install if not self.check_package(pack)]
+        to_install = [pack for pack in all_package_to_install if not utils.check_rpm_package_exists(pack)]
         to_install = " ".join(to_install)
         if subprocess.call("pkexec dnf install {} -y --best".format(to_install),shell=True)==0:
             return True
         return False
         
     def remove(self):
-        to_remove = " ".join([pack for pack in all_package_to_remove if self.check_package(pack)])
+        to_remove = " ".join([pack for pack in all_package_to_remove if utils.check_rpm_package_exists(pack)])
         if subprocess.call("pkexec rpm -v --nodeps -e {}".format(to_remove),shell=True)==0:
             return True
         return False
 
-    def check_package(self,package_name):
-        if subprocess.call("rpm -q {} &>/dev/null".format(package_name),shell=True) == 0:
-            return True
-        return False
         
