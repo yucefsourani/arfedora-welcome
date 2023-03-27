ae32db8f67771ff5237bc10efe86fd6f59981b17
yucef sourani <yucefsourani@users.noreply.github.com>
2023-03-27 09:16:44 +0300

    Update darktable_fedora.py

--- src/xxcx/darktable_fedora.py
+++ src/xxcx/darktable_fedora.py
@@ -21,35 +21,35 @@
 #  MA 02110-1301, USA.
 #  
 #  
-from universalplugin.uplugin import BasePlugin
-from utils import get_uniq_name,write_to_tmp
+from arfedora_welcome.classesplugin import BasePlugin
+from arfedora_welcome.utils import get_uniq_name
 import subprocess
-import time
 import os
 
 if_true_skip         = False
-if_false_skip        = True
-if_one_true_skip     = [False,False]
-if_all_true_skip     = [True,False]
+type_                = "installer"
+arch                 = ("all",)
+distro_name          = ("fedora",)
+distro_version       = ("all",)
+category             = "Graphics"
+category_icon_theme  = "applications-graphics-symbolic"
+desktop_env          = ("all",)
+display_type         = ("all",)
+title                = "Darktable"
+subtitle             = "Organize and develop images from digital cameras"
+keywords             = "darktable animation"
+licenses             = (("License\nGPL v3.0+","https://www.gnu.org/licenses/gpl-3.0.html"),)
+website              = ("WebSite","https://www.darktable.org/")
                 
-arch                 = ["all"]
-distro_name          = ["fedora"]
-distro_version       = ["all"]
-category             = "<b>Graphics</b>"
-category_icon_theme  = "applications-graphics"
 
 class Plugin(BasePlugin):
     __gtype_name__ = get_uniq_name(__file__) #uniq name and no space
-    def __init__(self,parent):
+    def __init__(self,parent,threads):
         BasePlugin.__init__(self,parent=parent,
-                            spacing=2,
-                            margin=10,
+                            threads=threads,
                             button_image="darktable.png",
-                            button_install_label="Install Darktable",
-                            button_remove_label="Remove Darktable",
-                            buttontooltip="Utility to organize and develop raw images",
-                            buttonsizewidth=100,
-                            buttonsizeheight=100,
+                            button_install_label="Install",
+                            button_remove_label="Remove",
                             button_frame=False,
                             blockparent=False,
                             daemon=True,
@@ -58,7 +58,7 @@ class Plugin(BasePlugin):
                             loadingmsg="Loading...",
                             ifinstallfailmsg="Install Darktable Failed",
                             ifremovefailmsg="Remove Darktable Failed",
-                            expand=False)
+                            parallel_install=False)
 
 
     def check(self):
