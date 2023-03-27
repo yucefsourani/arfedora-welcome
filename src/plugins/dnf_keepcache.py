1076a102281211b571e53b2af34b9badb61c5ee1
Yucef Sourani <youssef.m.sourani@gmail.com>
2023-03-27 03:39:58 +0300

    up

--- src/plugins/dnf_keepcache.py
+++ src/plugins/dnf_keepcache.py
@@ -21,21 +21,25 @@
 #  MA 02110-1301, USA.
 #  
 #  
-from universalplugin.uplugin import BasePlugin
-from utils import get_uniq_name,write_to_tmp
+from arfedora_welcome.classesplugin import BasePlugin
+from arfedora_welcome.utils import get_uniq_name
 import subprocess
-import os
+
 
 if_true_skip         = False
-if_false_skip        = True
-if_one_true_skip     = [False,False]
-if_all_true_skip     = [True,False]
-                
-arch                 = ["all"]
-distro_name          = ["fedora"]
-distro_version       = ["all"]
-category             = "<b>Utils</b>"
-category_icon_theme  = "preferences-other"
+type_                = "Enable/Disable"
+arch                 = ("all",)
+distro_name          = ("fedora",)
+distro_version       = ("all",)
+category             = "Utils"
+category_icon_theme  = "applications-utilities-symbolic"
+desktop_env          = ("all",)
+display_type         = ("all",)
+title                = "Dnf Keepcache"
+subtitle             = "Dnf Enable/Disable keepcache"
+keywords             = "dnf"
+licenses             = ()
+website              = ()
 
 
 true  = ["True", "true", "1"]
@@ -43,23 +47,19 @@ false = ["False", "false", "0"]
 
 class Plugin(BasePlugin):
     __gtype_name__ = get_uniq_name(__file__) #uniq name and no space
-    def __init__(self,parent):
+    def __init__(self,parent,threads):
         BasePlugin.__init__(self,parent=parent,
-                            spacing=2,
-                            margin=10,
+                            threads=threads,
                             button_image="rpm.svg",
-                            button_install_label="Dnf Enable keepcache",
-                            button_remove_label="Dnf Disable keepcache",
-                            buttontooltip="Enable Disable keepcache",
-                            buttonsizewidth=100,
-                            buttonsizeheight=100,
+                            button_install_label="Enable",
+                            button_remove_label="Disable",
                             button_frame=False,
                             blockparent=False,
                             daemon=True,
                             waitmsg="Wait...",
                             runningmsg="Running...",
                             loadingmsg="Loading...",
-                            expand=False)
+                            parallel_install=True)
 
 
     def check(self):
