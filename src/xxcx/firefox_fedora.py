c6f310f4bfb013f50152f9021a56071d5ce35053
yucef sourani <yucefsourani@users.noreply.github.com>
2023-03-27 09:35:02 +0300

    Update firefox_fedora.py

--- src/xxcx/firefox_fedora.py
+++ src/xxcx/firefox_fedora.py
@@ -21,35 +21,35 @@
 #  MA 02110-1301, USA.
 #  
 #  
-from universalplugin.uplugin import BasePlugin
-from utils import get_uniq_name,write_to_tmp
+from arfedora_welcome.classesplugin import BasePlugin
+from arfedora_welcome.utils import get_uniq_name
 import subprocess
-import time
 import os
 
 if_true_skip         = False
-if_false_skip        = True
-if_one_true_skip     = [False,False]
-if_all_true_skip     = [True,False]
-                
-arch                 = ["all"]
-distro_name          = ["fedora"]
-distro_version       = ["all"]
-category             = "<b>Internet</b>"
-category_icon_theme  = "applications-internet"
+type_                = "installer"
+arch                 = ("all",)
+distro_name          = ("fedora",)
+distro_version       = ("all",)
+category             = "Internet"
+category_icon_theme  = "web-browser-symbolic"
+desktop_env          = ("all",)
+display_type         = ("all",)
+title                = "Firefox"
+subtitle             = "Fast, Private & Safe Web Browser"
+keywords             = "firefox"
+licenses             = (("License\nMPL-2.0","https://www.mozilla.org/en-US/MPL/2.0/"),)
+website              = ("WebSite","https://www.mozilla.org/en-US/firefox/")
+
 
 class Plugin(BasePlugin):
     __gtype_name__ = get_uniq_name(__file__) #uniq name and no space
-    def __init__(self,parent):
+    def __init__(self,parent,threads):
         BasePlugin.__init__(self,parent=parent,
-                            spacing=2,
-                            margin=10,
+                            threads=threads,
                             button_image="appicns_Firefox.png",
-                            button_install_label="Install Firefox Browser",
-                            button_remove_label="Remove Firefox Browser",
-                            buttontooltip="Install Remove Firefox Browser",
-                            buttonsizewidth=100,
-                            buttonsizeheight=100,
+                            button_install_label="Install",
+                            button_remove_label="Remove",
                             button_frame=False,
                             blockparent=False,
                             daemon=True,
@@ -58,7 +58,7 @@ class Plugin(BasePlugin):
                             loadingmsg="Loading...",
                             ifinstallfailmsg="Install Firefox Browser Failed",
                             ifremovefailmsg="Remove Firefox Browser Failed",
-                            expand=False)
+                            parallel_install=False)
 
 
     def check(self):
