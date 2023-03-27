2f0a42638258bd7cb7a095944766ef51e1695b81
Yucef Sourani <youssef.m.sourani@gmail.com>
2023-03-27 04:02:20 +0300

    up

--- src/plugins/godot.py
+++ src/plugins/godot.py
@@ -21,42 +21,36 @@
 #  MA 02110-1301, USA.
 #  
 #  
-from universalplugin.uplugin import BasePlugin
-from utils import get_uniq_name,write_to_tmp
+from arfedora_welcome.classesplugin import BasePlugin
+from arfedora_welcome.utils import get_uniq_name,write_to_tmp
 import subprocess
-import time
-import os
-from urllib import request
-from gi.repository import GLib
-import subprocess
-import tempfile
-import queue
+
 
 if_true_skip         = False
-if_false_skip        = True
-if_one_true_skip     = [False,False]
-if_all_true_skip     = [True,False]
+type_                = "installer"
+arch                 = ("all",)
+distro_name          = ("all",)
+distro_version       = ("all",)
+category             = "Developer Tools"
+category_icon_theme  = "applications-science-symbolic"
+desktop_env          = ("all",)
+display_type         = ("all",)
+title                = "Godot"
+subtitle             = "Godot Game Engine editor\n(Flatpak User Wide)"
+keywords             = "godot game engine"
+licenses             = (("License\nMIT","https://opensource.org/license/mit/"),)
+website              = ("WebSite","https://godotengine.org/")
                 
-arch                 = ["all"]
-distro_name          = ["all"]
-distro_version       = ["all"]
-category             = "<b>Developer Tools</b>"
-category_icon_theme  = "applications-development"
-
 
 
 class Plugin(BasePlugin):
     __gtype_name__ = get_uniq_name(__file__) #uniq name and no space
-    def __init__(self,parent):
+    def __init__(self,parent,threads):
         BasePlugin.__init__(self,parent=parent,
-                            spacing=2,
-                            margin=10,
+                            threads=threads,
                             button_image="godot_icon.png",
-                            button_install_label="Install Godot (Flatpak User wide)",
-                            button_remove_label="Remove Godot (Flatpak User wide)",
-                            buttontooltip="Godot game engine editor",
-                            buttonsizewidth=100,
-                            buttonsizeheight=100,
+                            button_install_label="Install",
+                            button_remove_label="Remove",
                             button_frame=False,
                             blockparent=False,
                             waitmsg="Wait...",
@@ -68,7 +62,7 @@ class Plugin(BasePlugin):
                             ifremovesucessmsg="Remove Godot Done",
                             beforeinstallyesorno="Start Install Godot ?",
                             beforeremoveyesorno="Start Remove Godot ?",
-                            expand=False,
+                            parallel_install=False,
                             daemon=True)
 
         self.parent = parent
