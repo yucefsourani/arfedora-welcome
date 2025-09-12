3b6f61a090a5a059cc577270310aca480ab3c953
yucef sourani <yucefsourani@users.noreply.github.com>
2025-09-12 10:49:44 +0300

    Create compression_fedora_43.py

---
+++ src/plugins/compression_fedora_43.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  compression_fedora.py
+#  
+#  Copyright 2018 youcef sourani <youssef.m.sourani@gmail.com>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+from arfedora_welcome import utils
+from arfedora_welcome.classesplugin import BasePlugin
+from arfedora_welcome.utils import get_uniq_name,write_to_tmp
+import subprocess
+
+if_true_skip         = False
+type_                = "installer"
+arch                 = ("all",)
+distro_name          = ("fedora",)
+distro_version       = ("43","44","45","46")
+category             = "Utils"
+category_icon_theme  = "applications-utilities-symbolic"
+desktop_env          = ("all",)
+display_type         = ("all",)
+title                = "Compression Utility"
+subtitle             = "Utilities for listing/extracting/archiving files"
+keywords             = "compression extract zip unrar"
+licenses             = (("License\nUNKNOWN",""),)
+website              = ()
+                
+
+all_package =  ["zip", "gzip", "cpio","unar" , "7zip-standalone","7zip-standalone-all","7zip-reduced"]
+
+class Plugin(BasePlugin):
+    __gtype_name__ = get_uniq_name(__file__) #uniq name and no space
+    def __init__(self,parent,threads):
+        BasePlugin.__init__(self,parent=parent,
+                            threads=threads,
+                            button_image="tools_settings_tool_preferences-512.png",
+                            button_install_label="Install",
+                            button_remove_label="Remove",
+                            button_frame=False,
+                            blockparent=False,
+                            daemon=True,
+                            waitmsg="Wait...",
+                            runningmsg="Running...",
+                            loadingmsg="Loading...",
+                            ifinstallfailmsg="Install Compression Utility",
+                            ifremovefailmsg="Remove Compression Utility",
+                            parallel_install=False)
+
+
+    def check(self):
+        check_package = all([utils.check_rpm_package_exists(pack) for pack in all_package])
+        return not check_package
+        
+    def install(self):
+        to_install = [pack for pack in all_package if not utils.check_rpm_package_exists(pack)]
+        to_install = " ".join(to_install)
+        if subprocess.call("pkexec dnf install {} -y --best".format(to_install),shell=True)==0:
+            return True
+        return False
+        
+    def remove(self):
+        to_remove = " ".join([pack for pack in all_package if utils.check_rpm_package_exists(pack)])
+        if subprocess.call("pkexec rpm -v --nodeps -e {}".format(to_remove),shell=True)==0:
+            return True
+        return False
+        
